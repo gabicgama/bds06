288058eb8be042e9fe24f238e6ffc7ffd66eaf9d
gabriela <gabycgama@hotmail.com>
2022-04-30 20:26:23 -0300

    Movie page with pagination styling

--- front-movieflix/src/pages/Movies/index.tsx
+++ front-movieflix/src/pages/Movies/index.tsx
@@ -1,16 +1,90 @@
-import { Link } from "react-router-dom";
 import "./styles.css";
+import { useCallback, useEffect, useState } from "react";
+import { AxiosRequestConfig } from "axios";
+import { requestBackend } from "util/requests";
+import { SpringPage } from "types/vendor/spring";
+import { Link } from "react-router-dom";
+import { Movie } from "types/movie";
+import Pagination from "components/Pagination";
+import MovieFilter, { MovieFilterData } from "components/MovieFilter";
+
+type ControlComponentsData = {
+  activePage: number;
+  filterData: MovieFilterData;
+};
 
 const Movies = () => {
+  const [page, setPage] = useState<SpringPage<Movie>>();
+
+  const [controlComponentsData, setcontrolComponentsData] =
+    useState<ControlComponentsData>({
+      activePage: 0,
+      filterData: { genre: null },
+    });
+
+  const handlePageChange = (pageNumber: number) => {
+    setcontrolComponentsData({
+      activePage: pageNumber,
+      filterData: controlComponentsData.filterData,
+    });
+  };
+
+  const handleSubmitFilter = (data: MovieFilterData) => {
+    setcontrolComponentsData({
+      activePage: 0,
+      filterData: data,
+    });
+  };
+
+  const getMovies = useCallback(() => {
+    const config: AxiosRequestConfig = {
+      method: "GET",
+      url: "/movies",
+      withCredentials: true,
+      params: {
+        page: controlComponentsData.activePage,
+        size: 4,
+        genreId: controlComponentsData.filterData.genre?.id,
+      },
+    };
+    requestBackend(config).then((response) => {
+      setPage(response.data);
+    });
+  }, [controlComponentsData]);
+
+  useEffect(() => {
+    getMovies();
+  }, [getMovies]);
+
   return (
-    <div className="movies-container">
-      <h1>Tela listagem de filmes</h1>
-      <Link to="/movies/1">
-        <p>Acessar filme 1</p>
-      </Link>
-      <Link to="/movies/2">
-        <p>Acessar filme 2</p>
-      </Link>
+    <div className="container my-4 movie-container">
+      <MovieFilter onSubmitFilter={handleSubmitFilter} />
+      <div className="row">
+        {page?.content.map((movie) => (
+          <div className="col-sm-6 col-lg-4 col-xl-3" key={movie.id}>
+            <div className="base-card movie-card-container">
+              <Link to={"/movies/" + movie.id.toString()}>
+                <div className="movie-card-top-container">
+                  <img src={movie.imgUrl} alt={movie.title} />
+                </div>
+              </Link>
+              <div className="movie-card-bottom-container">
+                <h1>{movie.title}</h1>
+                <h2>{movie.year}</h2>
+                <p>{movie.subTitle}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="row movie-pagination-container">
+        <Pagination
+          pageCount={page ? page.totalPages : 0}
+          range={3}
+          onChange={handlePageChange}
+          forcePage={page?.number}
+        />
+      </div>
     </div>
   );
 };
